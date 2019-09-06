d5c08f1792656b05f031f29038119e4758ce90e3
Александр Зубрицкий <5998971@gmail.com>
2019-09-06 15:31:25 +0300

    fix build

--- gulpfile.js
+++ gulpfile.js
@@ -3,95 +3,35 @@
 var gulp = require("gulp");
 var plumber = require("gulp-plumber");
 var sourcemap = require("gulp-sourcemaps");
-var rename = require("gulp-rename");
 var less = require("gulp-less");
 var postcss = require("gulp-postcss");
 var autoprefixer = require("autoprefixer");
 var server = require("browser-sync").create();
-var csso = require("gulp-csso");
-var webp = require("gulp-webp");
-var imagemin = require("gulp-imagemin");
-var posthtml = require("gulp-posthtml");
-var include = require("posthtml-include");
-var del = require("del");
-var htmlmin = require("gulp-htmlmin");
-var uglify = require('gulp-uglify');
-var lzmajs = require('gulp-lzmajs');
 
-gulp.task("clean", function() {
-  return del("build");
-});
-
-gulp.task("copy", function() {
-  return gulp
-    .src(["source/fonts/**", "source/img/**", "source/*.ico"], {
-      base: "source"
-    })
-    .pipe(gulp.dest("build"));
-});
-
-gulp.task("css", function() {
-  return gulp
-    .src("source/less/style.less")
+gulp.task("css", function () {
+  return gulp.src("source/less/style.less")
     .pipe(plumber())
     .pipe(sourcemap.init())
     .pipe(less())
-    .pipe(postcss([autoprefixer()]))
-    .pipe(csso())
-    .pipe(rename("style.min.css"))
+    .pipe(postcss([
+      autoprefixer()
+    ]))
     .pipe(sourcemap.write("."))
-    .pipe(gulp.dest("build/css"));
+    .pipe(gulp.dest("source/css"))
+    .pipe(server.stream());
 });
 
-gulp.task("html", function() {
-  return gulp
-    .src("source/*.html")
-    .pipe(posthtml([include()]))
-    .pipe(htmlmin({ collapseWhitespace: true }))
-    .pipe(gulp.dest("build"));
-});
-
-gulp.task("compress", function() {
-  return gulp.src("source/js/*.js")
-             .pipe(uglify())
-             .pipe(lzmajs(9))
-             .pipe(gulp.dest("build/js"));
-});
-
-gulp.task("images", function() {
-  return gulp
-    .src("source/img/**/*{png,jpg,svg}")
-    .pipe(
-      imagemin([
-        imagemin.optipng({ optimizationLevel: 3 }),
-        imagemin.jpegtran({ progressive: true }),
-        imagemin.svgo()
-      ])
-    )
-    .pipe(gulp.dest("build/img"));
-});
-
-gulp.task("webp", function() {
-  return gulp
-    .src("source/img/**/*{png,jpg}")
-    .pipe(webp({ quality: 90 }))
-    .pipe(gulp.dest("build/img"));
-});
-
-gulp.task("server", function() {
+gulp.task("server", function () {
   server.init({
-    server: "build/"
+    server: "source/",
+    notify: false,
+    open: true,
+    cors: true,
+    ui: false
   });
 
-  gulp.watch("source/js/*.js", gulp.series("compress", "refresh"));
-  gulp.watch("source/less/**/*.less", gulp.series("css", "refresh"));
-  gulp.watch("source/*.html", gulp.series("html", "refresh"));
+  gulp.watch("source/less/**/*.less", gulp.series("css"));
+  gulp.watch("source/*.html").on("change", server.reload);
 });
 
-gulp.task("refresh", function(done) {
-  server.reload();
-  done();
-});
-
-gulp.task("build", gulp.series("clean", "copy", "css", "html", "compress"));
-gulp.task("start", gulp.series("build", "server"));
+gulp.task("start", gulp.series("css", "server"));
